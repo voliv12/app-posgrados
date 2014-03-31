c8fba4a68a12a805f93f53bd9ee41c9454611133
Víctor Olivares <voliv12@hotmail.com>
2014-03-31 11:26:03 -0600

    vista succes para login

---
+++ views/success_login.php
@@ -0,0 +1,8 @@
+<div class="alert alert-block alert-error fade in">
+    <h4 class="alert-heading">Error! </h4>  
+    <p><?php echo $mensaje; ?></p>      
+</div>
+
+<p><br />Page rendered in {elapsed_time} seconds</p>
+
+
