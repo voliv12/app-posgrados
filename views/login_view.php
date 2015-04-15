3dbacd7c3dac4e4edc35ee3766f728bd33cb8d69
Víctor Olivares <voliv12@hotmail.com>
2015-04-15 12:30:51 -0500

    Actualización general

--- views/login_view.php
+++ views/login_view.php
@@ -10,9 +10,9 @@
 
     <script type='text/javascript' src='js/jquery-1.8.2.js'></script>
     <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-    <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
-     <link href=" assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href=" assets/bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+     <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css"/>
 
     <!-- Le styles -->
 
