19c8360fc3d79e86100883ed8060ec4710dd9d25
Víctor Olivares <voliv12@hotmail.com>
2015-05-28 14:12:56 -0500

    Actualizacion a CVU alumnos

--- views/menu_principalalumno_view.php
+++ views/menu_principalalumno_view.php
@@ -7,13 +7,13 @@
 <div class="container well">
     <div class="row">
         <p>
-        <div class="span6"><a href="curso/calificacion_alumno" class="btn btn-block"><i class="icon-ok"></i> Calificaciones </a></div> 
+        <div class="span6"><a href="curso/calificacion_alumno" class="btn btn-block"><i class="icon-ok"></i> Calificaciones </a></div>
         <div class="span6"><a href="alumno" class="btn btn-block"><i class="icon-list"></i> Currículum Vitae Único</a></div>
         </p>
     </div>
     <div class="row">
             <p>
-            <div class="span6"><a href="http://www.uv.mx/ics/general/agenda-ics/" class="btn btn-block"><i class="icon-bullhorn"></i> Eventos</a></div>
+            <div class="span6"><a href="http://www.uv.mx/ics/general/agenda-ics/" class="btn btn-block" target="_blank"><i class="icon-bullhorn"></i> Eventos</a></div>
 
             </p>
     </div>
